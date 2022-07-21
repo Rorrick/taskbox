b4f74f098aa90d84fe79ad0f9c9abd56628b95ee
Rorrick Smith <rorrick@bulksms.com>
2022-07-21 10:08:34 +0200

    change task background to red

--- src/app/components/task.component.ts
+++ src/app/components/task.component.ts
@@ -32,7 +32,7 @@ import { Task } from '../models/task.model';
           id="title-{{ task?.id }}"
           name="title-{{ task?.id }}"
           placeholder="Input title"
-          style="background: blue;"
+          style="text-overflow: ellipsis;"
         />
       </label>
       <button
