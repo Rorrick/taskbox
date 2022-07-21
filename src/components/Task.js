e6e9ad852914739afaecc34db2b927ccf99d4138
Rorrick Smith <rorrick@bulksms.com>
2022-07-21 09:02:39 +0200

    change task background to red

---
+++ src/components/Task.js
@@ -0,0 +1,10 @@
+<label htmlFor="title" aria-label={title} className="title">
+<input
+  type="text"
+  value={title}
+  readOnly={true}
+  name="title"
+  placeholder="Input title"
+  style={{ background: 'red' }}
+  />
+</label>
